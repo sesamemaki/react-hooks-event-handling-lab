2ada66e9cc1dfe7b02d465b6699b9e5dfab2a450
Burcu Bayraö <burju7@yahoo.com>
2023-06-08 14:17:38 +0300

    completed assigment

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,11 @@
 // Code Keypad Component Here
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+function Keypad() {
+  return (
+    <div>
+      <input type="password" onChange={()=>console.log("Entering password...")} />
+    </div>
+  );
 }
 
 export default Keypad;
